e6bbdb618d638311d37ef7ae9497424e0899d1c1
liuyupian <yige.ss@foxmail.com>
2025-11-27 16:51:06 +0800

    Delete src directory

--- src/main/java/com/lyp/cdspringboot/mapper/CourseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lyp.cdspringboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lyp.cdspringboot.entity.Course;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CourseMapper extends BaseMapper<Course> {
-}
--- src/main/java/com/lyp/cdspringboot/mapper/ReportReviewMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lyp.cdspringboot.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lyp.cdspringboot.entity.ReportReview;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ReportReviewMapper extends BaseMapper<ReportReview> {
-}
